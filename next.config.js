fde4f02f509cef8aa2eb159eed7135200f17105f
andrew <aihof757@gmail.com>
2024-11-13 23:38:19 -0500

    test

--- next.config.js
+++ next.config.js
@@ -1,1 +1,10 @@
-module.exports = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+      NEXT_PUBLIC_WEAVIATE_URL: 'https://2d4pu317qfmkic5x70saa.c0.us-east1.gcp.weaviate.cloud',
+      NEXT_PUBLIC_WEAVIATE_API_KEY: 'GXKjbzlUUP53DtLYhelXeSNTs0Pf7E4rokyZ',
+    },
+  };
+  
+  module.exports = nextConfig;
+  
--- real-estate-frontend/src/app/api/test/route.ts
+++ real-estate-frontend/src/app/api/test/route.ts
@@ -3,3 +3,4 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   return NextResponse.json({ message: 'Test route working' });
 }
+//testing something
